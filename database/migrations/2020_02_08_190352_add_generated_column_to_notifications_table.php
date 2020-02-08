15fc4c88215de664ab9a609e015e7a5173b53adc
birendra <birendragurung007@gmail.com>
2020-02-09 01:13:31 +0545

    notifications setup

---
+++ database/migrations/2020_02_08_190352_add_generated_column_to_notifications_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class AddGeneratedColumnToNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('notifications', function (Blueprint $table) {
+            //`column_name` datatype GENERATED ALWAYS AS (expression)
+            DB::unprepared('ALTER TABLE `notifications` add `names_virtual` varchar(20) GENERATED ALWAYS  AS (`extra`->> "$.name")');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('notifications', function (Blueprint $table) {
+            $table->dropColumn('names_virtual');
+        });
+    }
+}
