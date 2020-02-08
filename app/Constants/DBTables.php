6b3c57ff063368eda5f77b450a89243b3b1c2635
birendra <birendragurung007@gmail.com>
2020-02-08 17:49:26 +0545

    added table name constants

--- app/Constants/DBTables.php
+++ app/Constants/DBTables.php
@@ -7,5 +7,9 @@ namespace App\Constants;
 class DBTables
 {
 
-    const TASKS = "tasks";
+    const TASKS = 'tasks';
+
+    const USERS = 'users';
+
+    const VERIFICATION_TOKEN = 'verification_tokens';
 }
